4d11ab90bf753e6c879b924e236ecda2cf2a4f72
christophe <65019992+Stolidbug@users.noreply.github.com>
2021-03-04 15:49:16 +0100

    Context change of channel

---
+++ src/Context/TimeBasedChannelContext.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Context;
+
+
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+
+final class TimeBasedChannelContext implements ChannelContextInterface
+{
+    /** @var ChannelRepositoryInterface */
+
+    private $channelRepository;
+
+
+    public function __construct(ChannelRepositoryInterface $channelRepository)
+    {
+        $this->channelRepository = $channelRepository;
+    }
+
+    public function getChannel(): ChannelInterface
+    {
+        if (!$this->isNight())
+        {
+            return  $this->channelRepository->findOneByCode('NIGHT');
+        }
+        return $this->channelRepository->findOneBy([]);
+    }
+
+    private function isNight(): bool
+    {
+        $currentHour = (int) (new \DateTime())->format('H');
+
+        return $currentHour > 19 || $currentHour < 6;
+    }
+}
