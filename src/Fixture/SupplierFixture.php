282a875b9fbab9148f7c53a276cf3af222d7cedb
christophe <65019992+Stolidbug@users.noreply.github.com>
2021-03-10 15:17:50 +0100

    Customizing Fixtures

---
+++ src/Fixture/SupplierFixture.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Fixture;
+
+
+use App\Entity\Supplierinterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Generator;
+use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
+use Sylius\Bundle\FixturesBundle\Fixture\FixtureInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+final class SupplierFixture extends AbstractFixture implements FixtureInterface
+{
+    /** @var FactoryInterface */
+    private $supplierFactory;
+
+    /** @var ObjectManager */
+    private $supplierManager;
+
+    /** @var Generator */
+    private $generator;
+
+    /**
+     * SupplierFixture constructor.
+     * @param FactoryInterface $supplierFactory
+     * @param ObjectManager $supplierManager
+     * @param Generator $generator
+     */
+    public function __construct(FactoryInterface $supplierFactory, ObjectManager $supplierManager, Generator $generator)
+    {
+        $this->supplierFactory = $supplierFactory;
+        $this->supplierManager = $supplierManager;
+        $this->generator = $generator;
+    }
+
+    public function load(array $options): void
+    {
+        for ($i= 0; $i < $options['count']; $i++)
+        {
+            /** @var Supplierinterface $supplier */
+            $supplier = $this->supplierFactory->createNew();
+            $supplier->setEmail($this->generator->companyEmail);
+            $supplier->setName($this->generator->company);
+            $this->supplierManager->persist($supplier);
+        }
+        $this->supplierManager->flush();
+    }
+
+    public function getName(): string
+    {
+        return 'supplier';
+    }
+
+    protected function configureOptionsNode(ArrayNodeDefinition $optionsNode): void
+    {
+        $optionsNode
+            ->children()
+            ->integerNode('count')
+            ;
+    }
+}
