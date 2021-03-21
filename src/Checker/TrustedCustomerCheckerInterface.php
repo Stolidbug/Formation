d3c324d39c05b2b0e3f475d84d4b3ca5e38b49c6
christophe <65019992+Stolidbug@users.noreply.github.com>
2021-03-21 15:00:36 +0100

    Automatic review approval

---
+++ src/Checker/TrustedCustomerCheckerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Checker;
+
+
+use Sylius\Component\Core\Model\CustomerInterface;
+
+interface TrustedCustomerCheckerInterface
+{
+    public function check(CustomerInterface $customer): bool;
+}
